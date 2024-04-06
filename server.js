c956260ecca5665ea507b770939a827b6b450f3e
Kainat rasheed <59202827+kainatrasheedprojects@users.noreply.github.com>
2024-04-06 11:33:37 -0600

    Update server.js

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
 import path from "path";
+import { fileURLToPath } from "url";
 
 //configure env
 dotenv.config();
@@ -22,7 +23,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
-app.use{express.static(path.join(__dirname, './client/build'))}
+app.use(express.static(path.join(__dirname, './client/build')));
 
 //routes
 app.use("/api/v1/auth", authRoutes);
@@ -33,7 +34,9 @@ app.use("/api/v1/product", productRoutes);
 app.use('*',function(req,res){
   res.sendfile(path.join(__dirname,'./client/build/index.html'));
 });
-
+//esmodulefix
+const __filename=fileURLToPath(import.meta.url);
+const __dirname=path.dirname(__filename);
 //PORT
 const PORT = process.env.PORT || 8080;
 
